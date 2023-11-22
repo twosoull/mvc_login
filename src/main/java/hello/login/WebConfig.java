339ff66e47a23f124d70fd5c898c3694aece26b1
iyeonghun <twosoull@naver.com>
2023-11-22 22:13:02 +0900

    필터와 인터셉터 등록

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -1,17 +1,29 @@
 package hello.login;
 
+import hello.login.web.Interceptor.LogInterceptor;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {//WebMvcConfigurer은 인터셉터 등록 시 필요
 
-    @Bean
+    // 오버라이드 단축키 command + N
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")//허용 패턴
+                .excludePathPatterns("/css/**","/*.ico","/error"); //미허용 패턴
+    }
+
+ //   @Bean
     public FilterRegistrationBean logFilter(){
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter()); //만든 필터를 넣는다.
@@ -22,7 +34,7 @@ public class WebConfig {
 
     }
 
-    @Bean
+    //@Bean
     public FilterRegistrationBean loginFilter(){
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LoginCheckFilter()); //만든 필터를 넣는다.
